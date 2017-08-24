1e72ce9d76ec6089aadc365eea9eea9e5402967c
Lucas <viper20@poczta.fm>
2017-08-24 13:26:55 +0200

    fixed intendation

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,32 @@
-const baseUrl = 'https://kodilla.com/pl/bootcamp-api';
-const myHeaders = {
-  'X-Client-Id': '2089',
-  'X-Auth-Token': '29fc49f42324fa3816bfc12b7942bf82'
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': '2089',
+    'X-Auth-Token': '29fc49f42324fa3816bfc12b7942bf82'
 };
 
 $.ajaxSetup({
-  headers: myHeaders
+    headers: myHeaders
 });
 
 $.ajax({
-  url: baseUrl + '/board',
-  method: 'GET',
-  success: function (response) {
-    setupColumns(response.columns);
-  }
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function (response) {
+        setupColumns(response.columns);
+    }
 });
 
-function setupColumns (columns) {
-  columns.forEach(function(column) {
-    var col = new Column(column.id, column.name);
-    board.createColumn(col);
-    setupCards(col, column.cards);
-  });
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
 }
 
 function setupCards (col, cards) {
-  cards.forEach(function (card) {
-    var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-    col.createCard(card);
-  });
+    cards.forEach(function (card) {
+        card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+        col.createCard(card);
+    });
 }
--- scripts/column.js
+++ scripts/column.js
@@ -1,59 +1,59 @@
-function Column (id, name) {
-  var self = this;
+function Column(id, name) {
+    var self = this;
 
-  this.id = id;
-  this.name = name || 'No name given';
-  this.element = createColumn();
+    this.id = id;
+    this.name = name || 'No name given';
+    this.element = createColumn();
 
-  function createColumn () {
+    function createColumn() {
+        var column = $('<div class="column"></div>');
+        var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
+        var columnCardList = $('<ul class="card-list"></ul>');
+        var columnDelete = $('<button class="btn-delete">x</button>');
+        var columnAddCard = $('<button class="column-add-card">Add card</button>');
 
-    var column = $('<div class="column"></div>');
-    var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
-    var columnCardList = $('<ul class="card-list"></ul>');
-    var columnDelete = $('<button class="btn-delete">x</button>');
-    var columnAddCard = $('<button class="column-add-card">Add card</button>');
+        columnDelete.click(function () {
+            self.deleteColumn();
+        });
 
-    columnDelete.click(function () {
-      self.deleteColumn();
-    });
+        columnAddCard.click(function (event) {
+            var cardName = prompt ('Enter the name of the card');
+            event.preventDefault();
+            $.ajax({
+                url: baseUrl + '/card',
+                method: 'POST',
+                data: {
+                    name: cardName,
+                    bootcamp_kanban_column_id: self.id
+                },
+                success: function (response) {
+                    var card = new Card(response.id, cardName);
+                    self.createCard(card);
+               }
+           });
+        });
 
-    columnAddCard.click(function (event) {
-      var cardName = prompt ('Enter the name of the card');
-      event.preventDefault();
-      $.ajax({
-        url: baseUrl + '/card',
-        method: 'POST',
-        data: {
-          name: cardName,
-          bootcamp_kanban_column_id: self.id
-        },
-        success: function (response) {
-          var card = new Card(response.id, cardName);
-          self.createCard(card);
-        }
-      });
-    });
-
-    column.append(columnTitle)
+        column.append(columnTitle)
           .append(columnDelete)
           .append(columnAddCard)
           .append(columnCardList);
-    return column;
-  }
+        return column;
+    }
 }
+
 Column.prototype = {
-  createCard: function (card) {
-    this.element.children('ul').append(card.element);
-  },
-  deleteColumn: function () {
-    var self = this;
-    $.ajax({
-      url: baseUrl + '/column/' + self.id,
-      method: 'DELETE',
-      success: function (response) {
-        self.element.remove();
-      }
-    });
-    this.element.remove();
-  }
+    createCard: function (card) {
+        this.element.children('ul').append(card.element);
+    },
+    deleteColumn: function () {
+        var self = this;
+        $.ajax({
+            url: baseUrl + '/column/' + self.id,
+            method: 'DELETE',
+            success: function (response) {
+                self.element.remove();
+            }
+       });
+       this.element.remove();
+    }
 };
